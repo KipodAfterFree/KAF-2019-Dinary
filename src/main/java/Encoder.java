88e169a849f16abdc542116ff01fa3d66f57f764
Nadav Tasger <tashernadav@gmail.com>
2019-11-29 17:49:45 +0200

    Initial commit

---
+++ src/main/java/Encoder.java
@@ -0,0 +1,58 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+
+public class Encoder {
+
+    private static File ROOT_DIRECTORY = new File(System.getProperty("user.dir"));
+
+    public static void main(String[] args) {
+        int width = 250;
+        int height = 250;
+
+        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        Graphics2D g2d = bufferedImage.createGraphics();
+
+        ArrayList<Boolean> bits = toBinary(new File(new File(ROOT_DIRECTORY, "binary"), "binary"));
+
+        for (int i = 0; i < bits.size(); i++) {
+            int x = i % width;
+            int y = (i - x) / width;
+            g2d.setColor(bits.get(i) ? Color.black : Color.white);
+            g2d.drawLine(x, y, x, y);
+        }
+
+        // fill all the image with white
+        g2d.setColor(Color.white);
+
+        g2d.dispose();
+
+        File file = new File(new File(ROOT_DIRECTORY, "dinary"), "dinary.png");
+        try {
+            ImageIO.write(bufferedImage, "png", file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static ArrayList<Boolean> toBinary(File file) {
+        try {
+            byte[] bytes = Files.readAllBytes(file.toPath());
+            ArrayList<Boolean> bits = new ArrayList<>();
+            for (byte b : bytes) {
+                for (int i = 0; i < 8; i++) {
+                    bits.add((b << i & 1) == 1);
+                }
+            }
+            return bits;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+}
